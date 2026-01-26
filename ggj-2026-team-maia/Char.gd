aaae4156da852e6209e89de3a4e65a750a36041f
@karlProbst <karlprobst@protonmail.com>
2026-01-26 15:31:59 -0300

    animation player, some new teachings

--- ggj-2026-team-maia/Char.gd
+++ ggj-2026-team-maia/Char.gd
@@ -1,1 +1,14 @@
 extends CharacterBody2D
+
+var speed:float=300.0
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"): 
+		self.position.x+=delta*speed
+	if Input.is_action_pressed("move_left"): 
+		self.position.x-=delta*speed
+	self.skew+=10*delta
+
+func _explode():
+	self.queue_free()
+	
--- ggj-2026-team-maia/background.gd
+++ ggj-2026-team-maia/background.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var cloud_node:=$Clouds/TextureRect/Clouds
 @onready var ocean_node:=$Ocean/Ocean
 @onready var waves_node:=$Ocean/WavesMask/Waves
+
 var wave_sin:float=0.0
 func _process(delta: float) -> void:
 	cloud_node.texture.noise.offset.x+=delta*10
@@ -10,3 +11,4 @@ func _process(delta: float) -> void:
 	waves_node.texture.noise.offset.y=sin(wave_sin)*5
 	waves_node.texture.noise.offset.x+=delta*5
 	waves_node.texture.noise.frequency=0.0134+(sin(wave_sin)/4000)
+	
